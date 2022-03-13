14b66d56ce9a2eeb970089d94e8f775436cc8b41
lm101845 <1018457683@qq.com>
2022-03-13 10:27:42 +0800

    重新配环境失败，我先不学了，尼玛

--- 02-code/myApp/.history/App_20220221220409.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-const index = () => <View>
-    <Text>JSX</Text>
-</View>
--- 02-code/myApp/.history/App_20220221220419.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-const index = () => <View>
-    <Text>JSX</Text>
-</View>
-
-export default Index
